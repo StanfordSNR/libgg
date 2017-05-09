cb5d43ebec420750a837940f3ccaea10c80f1779
Dan Iter <daniter@stanford.edu>
2017-05-09 14:48:10 -0700

    dont print to stdout in gg.c

--- src/gg/gg.c
+++ src/gg/gg.c
@@ -7,7 +7,7 @@ char *get_gg_file( const char *filename ){
   char env_name[100];
   strcpy(env_name, filename);
 
-  printf("DANITER DEBUG : %s\n", filename);
+  fprintf(stderr, "DANITER DEBUG : %s\n", filename);
 
   for( char *c = env_name; *c != '\0'; c++){
     char* dot = strchr( c, '.' );
