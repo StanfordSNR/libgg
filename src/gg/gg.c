b781aea8a2a4c0620a7919a85b090d9460300f91
Dan Iter <daniter@stanford.edu>
2017-05-09 14:19:43 -0700

    have to modify the file names a little for env variables to work

--- src/gg/gg.c
+++ src/gg/gg.c
@@ -1,6 +1,25 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "gg.h"
 
 char *get_gg_file( const char *filename ){
-  return getenv( filename );
+  char env_name[100];
+  strcpy(env_name, filename);
+
+  printf("DANITER DEBUG : %s\n", filename);
+
+  for( char *c = env_name; *c != '\0'; c++){
+    char* dot = strchr( c, '.' );
+    if( dot != NULL ){
+      *dot = '_';
+      c = dot;
+    }
+    dot = strchr( c, '/' );
+    if( dot != NULL ){
+      *dot = '_';
+      c = dot;
+    }
+  }
+  return getenv( env_name );
 }
