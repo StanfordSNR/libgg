dbf4f6bec048cd96fd50750f1ad480cc65628d51
Dan Iter <daniter@stanford.edu>
2017-05-11 08:33:01 -0700

    removing debug statements and adding support for dash

--- src/gg/gg.c
+++ src/gg/gg.c
@@ -7,16 +7,11 @@ char *get_gg_file( const char *filename ){
   char env_name[100];
   strcpy(env_name, filename);
 
-  fprintf(stderr, "DANITER DEBUG : %s\n", filename);
+  //fprintf(stderr, "DANITER DEBUG : %s\n", filename);
 
   for( char *c = env_name; *c != '\0'; c++){
-    char* dot = strchr( c, '.' );
-    if( dot != NULL ){
-      *dot = '_';
-      c = dot;
-    }
-    dot = strchr( c, '/' );
-    if( dot != NULL ){
+    char *dot = NULL;
+    if( ( dot = strchr( c, '.' ) ) || ( dot = strchr( c, '/' ) ) || ( dot = strchr( c, '-' ) ) ){
       *dot = '_';
       c = dot;
     }
