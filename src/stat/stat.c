68e122ab98aa6cb7b9ff7133614cc08fbe32b959
Dan Iter <daniter@stanford.edu>
2017-05-11 19:17:48 -0700

    adding stat

--- src/stat/stat.c
+++ src/stat/stat.c
@@ -1,10 +1,23 @@
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
+
 #include "syscall.h"
 #include "libc.h"
+#include "gg.h"
 
 int stat(const char *restrict path, struct stat *restrict buf)
 {
+	if( getenv( GG_ENV_VAR ) ) {
+		char *new_file = get_gg_file(path);
+		if (NULL != new_file) {
+      path = new_file;
+		} else {
+			fprintf(stderr, "DANITER ACCESS DENIED : %s\n", path);
+			return ENOENT;
+		}
+	}
+
 #ifdef SYS_stat
 	return syscall(SYS_stat, path, buf);
 #else
