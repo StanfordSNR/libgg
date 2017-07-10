935da6ef26181609d3d917ce27bd1b4a88b121b1
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-09 18:19:32 -0700

    Add a short comment for get_gg_file function.

--- include/gg.h
+++ include/gg.h
@@ -1,11 +1,8 @@
 #ifndef _GG_H_
 #define _GG_H_
 
-/*
- * This checks the ENV for a mapping from the filename to the
- * hash of the file, which is the name of the file in the .gg directory.
- */
-
+/* Please note that the user is responsible to free the memory for the resulting
+   string */
 char * get_gg_file( const char * filename );
 
 #define GG_THUNK_MAGIC_NUMBER "##GGTHUNK##"
