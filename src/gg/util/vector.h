b885aa4378ddb2dc8e8a764c7764dca1a7f712b3
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-09 18:17:21 -0700

    Make the vector implementation generic.

---
+++ src/gg/util/vector.h
@@ -0,0 +1,47 @@
+#ifndef GG_VECTOR_H
+#define GG_VECTOR_H
+
+#define INITIAL_SIZE 10
+
+#define VECTOR(x) typedef struct \
+{ \
+  size_t size; \
+  size_t count; \
+  x * data; \
+} vector_##x; \
+\
+void vector_##x##_init( vector_##x * v ) \
+{ \
+  v->data = NULL; \
+  v->size = 0; \
+  v->count = 0; \
+} \
+\
+void vector_##x##_push_back( vector_##x * v, x * d ) \
+{ \
+  if ( v->size == 0 ) { \
+    v->size = INITIAL_SIZE; \
+    v->data = malloc( sizeof( x ) * INITIAL_SIZE ); \
+  } \
+\
+  if ( v->size == v->count ) { \
+    v->size *= 2; \
+    v->data = realloc( v->data, sizeof( x ) * v->size ); \
+  } \
+\
+  v->data[ v->count++ ] = *d; \
+} \
+\
+x * vector_##x##_at( vector_##x * v, size_t index ) { \
+  if ( index >= v->count ) { \
+    return NULL; \
+  } \
+\
+  return &v->data[ index ]; \
+} \
+\
+void vector_##x##_free( vector_##x * v ) { \
+  free( v->data ); \
+}
+
+#endif /* GG_VECTOR_H */
