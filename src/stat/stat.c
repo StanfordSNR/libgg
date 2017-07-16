fd4f550bf2570b71fa11271ab3da3f1bf9a7cea7
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-16 00:55:57 -0700

    Returns dummy stat results if gg is enabled.

--- src/stat/stat.c
+++ src/stat/stat.c
@@ -8,25 +8,8 @@
 
 int stat(const char *restrict path, struct stat *restrict buf)
 {
-	int gg_fake_dir = -1;
-
 	if( __gg.enabled ) {
-		char * new_file = __gg_get_filename( path );
-
-		if ( NULL != new_file ) {
-				path = new_file;
-		}
-		else if ( ( gg_fake_dir = is_dir_allowed( path ) ) >= 0 ) {
-			path = __gg.dir;
-		}
-		else {
-			if( __gg.verbose ) {
-				fprintf( stderr, "[gg] stat() denied: %s\n", path );
-			}
-
-			errno = ENOENT;
-			return -1;
-		}
+		return __gg_stat( path, buf );
 	}
 
 	int retval;
@@ -35,12 +18,6 @@ int stat(const char *restrict path, struct stat *restrict buf)
 #else
 	retval = syscall(SYS_fstatat, AT_FDCWD, path, buf, 0);
 #endif
-
-	/* [gg] we assign a different inode num to each folder to make them appear as
-	   different directories */
-	if ( gg_fake_dir >= 0 ) {
-		buf->st_ino += ( gg_fake_dir + 1 );
-	}
 
 	return retval;
 }
