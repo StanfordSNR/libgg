f19f851fbcfb0c7623be03302484638852f144f5
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-16 17:17:50 -0700

    getcwd.c: in gg mode, return the dirname of the thunk (chdir is not allowed).

--- src/unistd/getcwd.c
+++ src/unistd/getcwd.c
@@ -2,13 +2,13 @@
 #include <errno.h>
 #include <limits.h>
 #include <string.h>
+#include <libgen.h>
 #include "syscall.h"
+
+#include "../gg/gg.h"
 
 char *getcwd(char *buf, size_t size)
 {
-    errno = EINVAL;
-    return 0;
-    // DANITER HACK TODO: rewrite
 	char tmp[PATH_MAX];
 	if (!buf) {
 		buf = tmp;
@@ -17,6 +17,18 @@ char *getcwd(char *buf, size_t size)
 		errno = EINVAL;
 		return 0;
 	}
-	if (syscall(SYS_getcwd, buf, size) < 0) return 0;
+
+	if (__gg.enabled) {
+		char * cwd = dirname(__gg.thunk_file);
+		if (strlen(cwd) + 1 > size) {
+			errno = ERANGE;
+			return NULL;
+		}
+		strcpy(buf, cwd);
+	}
+	else {
+		if (syscall(SYS_getcwd, buf, size) < 0) return 0;
+	}
+
 	return buf == tmp ? strdup(buf) : buf;
 }
