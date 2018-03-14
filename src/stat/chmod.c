a1d14cccfb30011d70cbc300c4028731346af264
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-13 18:11:25 -0700

    chmod.c: Allow chmod for output files.

--- src/stat/chmod.c
+++ src/stat/chmod.c
@@ -2,8 +2,17 @@
 #include <fcntl.h>
 #include "syscall.h"
 
+#include "../gg/gg.h"
+
 int chmod(const char *path, mode_t mode)
 {
+	if ( __gg.enabled ) {
+		Output * output = __gg_get_output( path );
+		if ( output ) {
+			path = output->tag;
+		}
+	}
+
 #ifdef SYS_chmod
 	return syscall(SYS_chmod, path, mode);
 #else
