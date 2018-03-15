ed0959bab34ab4e95353043cb405b48e795cbc70
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-14 22:50:33 -0700

    unlink.c: Only allow unlinking temp files.

--- src/unistd/unlink.c
+++ src/unistd/unlink.c
@@ -1,9 +1,25 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 #include "syscall.h"
+
+#include "../gg/gg.h"
 
 int unlink(const char *path)
 {
+	if ( __gg.enabled ) {
+		/* we only let programs unlink the temp files */
+		const char * allowed_file = __gg_get_allowed( path, false );
+
+		if ( path == NULL ) {
+			errno = EACCES;
+			return -1;
+		}
+
+		path = allowed_file;
+	}
+
+
 #ifdef SYS_unlink
 	return syscall(SYS_unlink, path);
 #else
